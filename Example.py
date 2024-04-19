1f7848ede17222e1f304a5f2b75ee96dbdd8a6c7
mamahoos <m.h.kkouhkan@gmail.com>
2024-04-19 14:38:55 +0330

    Refactored

--- Example.py
+++ Example.py
@@ -1,62 +1,50 @@
 from Property import Property
+from Classmethod import Classmethod
+from Staticmethod import Staticmethod
 
 
-class foo:
-	def __init__(self, value):
-		self.__value = value
-			
-	def getter(self):
-		print('Getting value')
-		return self.__value
-			
-	def setter(self, value):
-		print(f'Setting value to {value}')
-		self.__value = value
-
-	def deleter(self):
-		print('Deleting value')
-		del self.__value
-
-	value = Property(getter, setter, deleter, )
-	
-
-x = foo('meow')
-print(x.value)
-	
-x.value = 'meow again'
-print(x.value)
-
-del x.value
-try:    print(x.value)     
-except AttributeError as e:     print("AttributeError!")
-
-
-#-------#
-
-
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self._age = age
+class Example:
+    def __init__(self, value):
+        self._value = value
 
     @Property
-    def age(self):
-        print('Getting value')
-        return self._age
+    def value(self):
+        """Property decorator example: Get the value."""
+        print('- Getting value -')
+        return self._value
 
-    @age.setter
-    def age(self, value):
-        if isinstance(value, int) and 0 < value < 120:
-            print(f'Setting value to {value}')
-            self._age = value
-        else:
-            raise ValueError("Age must be an integer between 1 and 119")
-        
+    @value.setter
+    def value(self, new_value):
+        """Property decorator example: Set the value."""
+        print('- Setting value -')
+        self._value = new_value
 
-mamahoos = Person('mamahoos', 17)
+    @value.deleter
+    def value(self):
+        """Property decorator example: Delete the value."""
+        print('- Deleting value -')
+        del self._value
 
-mamahoos.age += 1		# ^_^
+    @Classmethod
+    def from_string(cls, string_value):
+        """Classmethod decorator example: Create an instance from a string."""
+        value = int(string_value)
+        return cls(value)
 
-print(
-    mamahoos.age
-)
+    @Staticmethod
+    def is_integer(value):
+        """Staticmethod decorator example: Check if a value is an integer."""
+        return isinstance(value, int)
+
+# Create an instance using the classmethod
+example_from_string = Example.from_string("10")
+
+# Access and modify the property
+print(f"Initial value: {example_from_string.value}")
+example_from_string.value = 20
+print(f"New value: {example_from_string.value}")
+del example_from_string.value
+
+# Use the staticmethod
+print(f"Is 5 an integer? {Example.is_integer(5)}")
+print(f"Is 'hello' an integer? {Example.is_integer('hello')}")
